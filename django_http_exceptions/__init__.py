de7983b9c0923f0f76dd88fce18bd0e7fd7f783a
Işık Kaplan <isik.kaplan@outlook.com>
2020-10-03 17:37:52 +0300

    Make raw exceptions importable from module directly

--- django_http_exceptions/__init__.py
+++ django_http_exceptions/__init__.py
@@ -1,4 +1,5 @@
 from .exceptions import HTTPExceptions
+from ._exceptions import * # Not adding this to __all__, there are 57 variables here
 from .middleware import (get_current_request, ExceptionHandlerMiddleware,
                          ThreadLocalRequestMiddleware)
 
